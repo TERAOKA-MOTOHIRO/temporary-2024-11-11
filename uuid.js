a83e821d979876dfb4691e97bf66ce025fd2920b
TERAOKA-MOTOHIRO <teramoto2283@gmail.com>
2024-11-11 03:05:39 +0000

    yorow

--- uuid.js
+++ uuid.js
@@ -1,3 +1,7 @@
-import { v4 } from "uuid";
-//UUID v4の文字列を出力
-console.log(v4());
+import { v4,v7 } from "uuid";
+//UUID v7の文字列を出力
+console.log(v7());
+
+export const createUuid=()=>{
+    return v7();
+};
